8b68ef2ce056e582285883bfabb916cc11aa0bea
1Baleid <s202245500@kfupm.edu.sa>
2026-04-10 20:16:47 +0300

    added root route

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -17,6 +17,9 @@ app.use(morgan("dev"));
 
 
 // TODO 6.1: Create root route "/"
+app.get("/", (req, res) => {
+  res.send("Welcome to the Quote API!");
+});
 
 
 // TODO 6.2: Create "/api/quote" route
